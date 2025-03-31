614c9880cd0de554306d3285f45483c5f99196fc
kaustabh-mishra <kaustabh.mishra@ext.airbnb.com>
2025-03-31 17:46:09 +0530

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import "@/styles/globals.css"; // Ensure Tailwind styles are loaded
-import Head from "next/head";
+import '@/styles/globals.css'; // Ensure Tailwind styles are loaded
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Air Bat N" Ball 2025</title>
+        <title>Air Bat N&apos; Ball 2025</title>
       </Head>
       <Component {...pageProps} />
     </>
